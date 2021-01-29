020ac67ae804b27558e0baa1c57cac0d2c04442a
shigo <shigorin_s@mail.ru>
2021-01-29 05:14:02 +0300

    red hw2

--- src/main/java/ru/hh/school/entity/Area.java
+++ src/main/java/ru/hh/school/entity/Area.java
@@ -10,7 +10,7 @@ public class Area {
   @Column(name = "area_id")
   private Integer id;
 
-  @Column(name = "area_name")
+  @Column(name = "name")
   private String name;
 
   public String getName() {
